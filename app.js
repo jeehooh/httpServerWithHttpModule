314877a436f02e6963d30de5fc7bbc1479d7bf1c
jeehooh <110149590+jeehooh@users.noreply.github.com>
2022-08-09 14:55:22 +0900

    [ADD] 유저 회원가입 엔드포인트 구현

---
+++ app.js
@@ -0,0 +1,47 @@
+const users = [
+  {
+    id: 1,
+    name: "Rebekah Johnson",
+    email: "Glover12345@gmail.com",
+    password: "123qwe",
+  },
+  {
+    id: 2,
+    name: "Fabian Predovic",
+    email: "Connell29@gmail.com",
+    password: "password",
+  },
+];
+
+const posts = [
+  {
+    id: 1,
+    title: "간단한 HTTP API 개발 시작!",
+    content: "Node.js에 내장되어 있는 http 모듈을 사용해서 HTTP server를 구현.",
+    userId: 1,
+  },
+  {
+    id: 2,
+    title: "HTTP의 특성",
+    content: "Request/Response와 Stateless!!",
+    userId: 1,
+  },
+];
+
+const createUser = (req, res) => {
+  const user = req.body.data;
+  console.log(user);
+
+  users.push({
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    password: user.password,
+  });
+
+  console.log("after: ", users);
+
+  res.json({ message: "userCreated" });
+};
+
+module.exports = { createUser };
